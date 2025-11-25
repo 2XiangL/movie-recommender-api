839ed5b1f53f1641c6c3f1bf96bc126b4c9d2eab
2XiangL <nzgbsm@outlook.com>
2025-11-25 08:12:26 +0800

    utils

---
+++ utils/common.py
@@ -0,0 +1,27 @@
+"""Common utility functions for the Movie Recommendation API."""
+
+import numpy as np
+
+
+def convert_numpy_types(obj):
+    """
+    Convert numpy data types to native Python data types.
+    
+    Args:
+        obj: Object that may contain numpy types
+        
+    Returns:
+        Object with numpy types converted to native Python types
+    """
+    if isinstance(obj, np.integer):
+        return int(obj)
+    elif isinstance(obj, np.floating):
+        return float(obj)
+    elif isinstance(obj, np.ndarray):
+        return obj.tolist()
+    elif isinstance(obj, dict):
+        return {key: convert_numpy_types(value) for key, value in obj.items()}
+    elif isinstance(obj, list):
+        return [convert_numpy_types(item) for item in obj]
+    else:
+        return obj
